95bdbd0076112579abfc917458f43c3236776224
AbolfazlAdhami <aadhami880@gmail.com>
2025-02-14 02:12:12 +0330

    new : user register actoins functions

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,9 +1,10 @@
 "use server";
 import { Query, ID } from "node-appwrite";
-import { users } from "../appwrite.config";
-import { CreateUserParams } from "@/types";
+import { users, database, storage, BUCKET_ID, DATABASE_ID, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID } from "../appwrite.config";
+import { CreateUserParams, RegisterUserParams } from "@/types";
 import { parseStringify } from "../utils";
 
+// CRETE  USER
 export const createUser = async (user: CreateUserParams) => {
   try {
     const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name);
@@ -20,6 +21,7 @@ export const createUser = async (user: CreateUserParams) => {
   }
 };
 
+//  GET USER
 export const getUser = async (userId: string) => {
   try {
     const user = await users.get(userId);
@@ -29,3 +31,28 @@ export const getUser = async (userId: string) => {
     console.log(error);
   }
 };
+
+// REGISTER PTIENT
+export const registerPatient = async ({ identificationDocument, ...patient }: RegisterUserParams) => {
+  try {
+    // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+    let file;
+    if (identificationDocument) {
+      const inputFile = identificationDocument && new File(identificationDocument?.get("blobFile"), identificationDocument?.get("fileName"));
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+
+    const newPatient = await database.createDocument(DATABASE_ID!, PATIENT_COLLECTION_ID!, ID.unique(), {
+      identificationDocument: file?.$id ? file?.$id : null,
+      identificationDocumentUrl: file?.$id ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}` : null,
+      ...patient,
+    });
+
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.error("An error occurred while creating a new patient:", error);
+  }
+};
+
+// GET PATIENT
+export const getPatient = async (userID: string) => {};
