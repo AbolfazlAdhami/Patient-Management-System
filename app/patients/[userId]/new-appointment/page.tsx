f82a272827d0a4126ca07747de3f1b8956b350b1
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-14 02:29:08 +0330

    chor(files): setup patients pages and files

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,5 @@
+const Appointment = () => {
+  return <div className="flex h-screen max-h-screen"></div>;
+};
+
+export default Appointment;
---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,5 @@
+const RequestSuccess = () => {
+  return <div className="flex h-screen max-h-screen px-[5%]"></div>;
+};
+
+export default RequestSuccess;
