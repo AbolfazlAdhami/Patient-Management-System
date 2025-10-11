716e0c3dcaa2183a31c8a1da36225aa606790ed8
AbolfazlAdhami <aadhami880@gmail.com>
2025-10-11 23:07:29 +0330

    feat: table column components done

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -9,9 +9,64 @@ import { Appointment } from "@/types/appwrite.types";
 
 import { AppointmentModal } from "../AppointmentModal";
 import { StatusBadge } from "../StatusBadge";
+ 
 
-function columns() {
-  return <div>columns</div>;
-}
+export const columns: ColumnDef<Appointment>[] = [
+  { header: "#", cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p> },
+  {
+    accessorKey: "patient",
+    header: "Patient",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p className="text-14-medium">{appointment.patient.name}</p>;
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
+        <div className="min-w-[115px]">
+          <StatusBadge status={appointment.status} />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p className="text-14-regular min-w-[100px]">{formatDateTime(appointment.schedule).dateTime}</p>;
+    },
+  },
+  {
+    accessorKey: "primaryPhysician",
+    header: "Docter",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      const doctor = Doctors.find((doctor: any) => doctor.name === appointment.primaryPhysician);
 
-export default columns;
+      return (
+        <div className="flex items-center gap-3">
+          <Image src={doctor?.image} alt="docter" width={100} height={100} className="size-8" />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    header: () => <div className="pl-4">Actions</div>,
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <div className="flex gap-1">
+        <AppointmentModal/>
+        <AppointmentModal/>
+      </div>;
+    },
+  },
+];
+
+
